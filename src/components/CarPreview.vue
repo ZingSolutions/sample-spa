4aa2f3098cd4de2cb3f359d138043c26de3e2e00
Paul Johnson <paul.johnson@prospectsoft.com>
2019-04-17 16:30:04 +0100

    Lots of work for the testing example

---
+++ src/components/CarPreview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>{{ carData.make }} - {{ carData.model }} - {{ carData.year }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({})
+export default class CarPreview extends Vue {
+  @Prop({ default: {} }) public carData!: {
+    make: string;
+    model: string;
+    year: string;
+  };
+}
+</script>
