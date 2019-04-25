6a432c537c9ab2205983be89a9e9dbf6eebe1c67
Paul Johnson <paul.johnson@prospectsoft.com>
2019-04-25 16:37:39 +0100

    added example of route with custom styling

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
-      <router-link to="/test">Test Page</router-link>
+      <router-link to="/test">Test Page</router-link>|
+      <router-link to="/test-green">Green Test Page</router-link>
     </div>
     <router-view/>
   </div>
